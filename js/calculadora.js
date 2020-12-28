9e0d3fa823f30103c062a9d49c51c5809c096644
XabierGallardo <javiergallard91@gmail.com>
2020-12-28 18:45:45 +0100

    Completadas operaciones aritméticas con el teclado

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,22 +1,11 @@
 //Variables iniciales, num_pantalla irá agregando números y se guardarán en el array fila_1 
 let num_pantalla = 0;
-let fila_1 = [];
-let fila_2 = [];
-
-
-
-//Función de reseteo de todos los valores
-function reset() {
-	num_pantalla = 0;
-	fila_1 = [];
-	fila_2 = [];
-
-	$("#pantalla-resultados").html(num_pantalla);
-}
-
+let fila_1 = 0;
+let fila_2 = 0;
 
 
 //Registro números con mouse
+/*
 $("#tecla-9").click(function() {
 	num_pantalla += "9";
 	$("#pantalla-resultados").html(num_pantalla.substring(1));
@@ -72,20 +61,20 @@ $("#tecla-punto").click(function() {
 });
 
 $("#tecla-sumar").click(function() {
-	console.log("sumar");
+	sumar();
 });
 
-$("#tecla-menos").click(function() {
-	console.log("restar");
+$("#tecla-restar").click(function() {
+	restar();
 });
 
 $("#tecla-multiplicar").click(function() {
-	console.log("multiplicar");
+	multiplicar();
 });
 
 $("#tecla-dividir").click(function() {
-	console.log("dividir");
-});
+	dividir();
+});*/
 
 
 
@@ -93,7 +82,7 @@ $("#tecla-dividir").click(function() {
 $(document).keydown(function() {
 	const tecla = event.keyCode;
 	const tecla_valor = event.key;
-	//console.log("Tecla: " + tecla_valor + " código: " + tecla);
+	//console.log("tecla " + tecla + " valor " + tecla_valor);
 
 	/* Códigos
 	/	111
@@ -111,28 +100,27 @@ $(document).keydown(function() {
 	3	99
 	0	96
 	.	110
-	Intro 13
-	*/
+	Intro 13*/
 	switch(tecla) {
 
 		case 107: //Tecla +
-			console.log("sumar");
+			sumar();
 		break;
 
 		case 109: //Tecla -
-			console.log("restar");
+			restar();
 		break;
 
 		case 106: //Tecla *
-			console.log("multiplicar");
+			multiplicar();
 		break;
 
 		case 111: //Tecla /
-			console.log("dividir");
+			dividir();
 		break;
 
 		case 13: //Tecla Intro
-			console.log("resultado");
+			resultado();
 		break;
 
 		case 110: //Tecla .
@@ -192,6 +180,10 @@ $(document).keydown(function() {
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
 
+		case 144: //Tecla bloq
+			reset();
+		break;
+
 		default:
 		console.log("Tecla incorrecta");
 	}
@@ -199,8 +191,195 @@ $(document).keydown(function() {
 
 
 
+//Función de reseteo de todos los valores
+function reset() {
+	num_pantalla = 0;
+	fila_1 = 0;
+	fila_2 = 0;
+
+	$("#pantalla-resultados").html(num_pantalla);
+	$("#pantalla-previa").html(num_pantalla);
+}
+
+
+
+function sumar() {
+	
+	//Convertimos en float el número de pantalla
+	let valor_pantalla = $("#pantalla-resultados").text();
+	let num_previo = parseFloat(valor_pantalla);
+	
+	//Si ya existía un valor previo
+	if(fila_1 !== 0) { 
+		
+		//Asignamos el nuevo valor a la fila_2
+		fila_2 = num_previo;
+
+		//Hacemos la suma
+		let resultado = fila_1 + fila_2;
+
+		//Imprimimos los valores
+		$("#pantalla-previa").html(resultado);
+		$("#pantalla-resultados").html(resultado);
+
+		//Pasamos el nuevo valor a la fila_1 y reseteamos los valores
+		fila_1 = resultado;
+		fila_2 = 0;
+		num_pantalla = 0;
+
+
+	} else {
+
+		//Metemos el nuevo valor en fila_1 e imprimimos
+		fila_1 = num_previo;
+		$("#pantalla-previa").html(num_previo);
+
+		num_pantalla = 0;
+		$("#pantalla-resultados").html(num_pantalla);
+
+	}
+}
+
+
+
+function restar() {
+	
+	let valor_pantalla = $("#pantalla-resultados").text();
+	let num_previo = parseFloat(valor_pantalla);
+	
+	if(fila_1 !== 0) { 
+		
+		fila_2 = num_previo;
+
+		let resultado = fila_1 - fila_2;
+
+		$("#pantalla-previa").html(resultado);
+		$("#pantalla-resultados").html(resultado);
+
+		fila_1 = resultado;
+		fila_2 = 0;
+		num_pantalla = 0;
+
+
+	} else {
+
+		fila_1 = num_previo;
+		$("#pantalla-previa").html(num_previo);
+
+		num_pantalla = 0;
+		$("#pantalla-resultados").html(num_pantalla);
+
+	}
+}
+
+
+
+function multiplicar() {
+	
+	let valor_pantalla = $("#pantalla-resultados").text();
+	let num_previo = parseFloat(valor_pantalla);
+	
+	if(fila_1 !== 0) { 
+		
+		fila_2 = num_previo;
+
+		let resultado = fila_1 * fila_2;
+
+		$("#pantalla-previa").html(resultado);
+		$("#pantalla-resultados").html(resultado);
+
+		fila_1 = resultado;
+		fila_2 = 0;
+		num_pantalla = 0;
+
+
+	} else {
+
+		fila_1 = num_previo;
+		$("#pantalla-previa").html(num_previo);
+
+		num_pantalla = 0;
+		$("#pantalla-resultados").html(num_pantalla);
+
+	}
+}
+
+
+
+function dividir() {
+	
+	let valor_pantalla = $("#pantalla-resultados").text();
+	let num_previo = parseFloat(valor_pantalla);
+	
+	if(fila_1 !== 0) { 
+		
+		fila_2 = num_previo;
+
+		let resultado = fila_1 / fila_2;
+
+		$("#pantalla-previa").html(resultado);
+		$("#pantalla-resultados").html(resultado);
+
+		fila_1 = resultado;
+		fila_2 = 0;
+		num_pantalla = 0;
+
+
+	} else {
+
+		fila_1 = num_previo;
+		$("#pantalla-previa").html(num_previo);
+
+		num_pantalla = 0;
+		$("#pantalla-resultados").html(num_pantalla);
+
+	}
+}
+
+
+
+function resultado() {
+/*
+	let valor_pantalla = $("#pantalla-resultados").text();
+	let num_previo = parseInt(valor_pantalla);
+	
+	if(fila_1 !== 0) { 
+		
+		fila_2 = num_previo;
+
+		//Para imprimir el resultado, sumamos un 0
+		let resultado = fila_1 + 0;
+
+		$("#pantalla-previa").html(resultado);
+		$("#pantalla-resultados").html(resultado);
+
+		fila_1 = resultado;
+		fila_2 = 0;
+		num_pantalla = 0;
+
+
+	} else {
+
+		fila_1 = num_previo;
+		fila_2 = 0;
+
+		let resultado = fila_1 + fila_2;
+
+		$("#pantalla-previa").html(resultado);
+		$("#pantalla-resultados").html(resultado);
+
+		fila_1 = resultado;
+		fila_2 = 0;
+		num_pantalla = 0;
+
+	}
+	*/
+}
+
+
+
 //Resetear la calculadora
-$("#tecla-bloq").click(function(){ 
+$("#tecla-bloq").click(function(){
 	reset();
 });
 
