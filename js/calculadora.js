43f4cc2a7f6687173a98e2d5d6dc473e0952892e
XabierGallardo <javiergallard91@gmail.com>
2021-01-21 21:08:05 +0100

    Añadido nuevo registro de teclas en teclado no numeral y mejor indentación

--- js/calculadora.js
+++ js/calculadora.js
@@ -5,7 +5,7 @@ let fila_2 = 0;
 
 
 //Registro números con mouse
-/*
+
 $("#tecla-9").click(function() {
 	num_pantalla += "9";
 	$("#pantalla-resultados").html(num_pantalla.substring(1));
@@ -74,33 +74,36 @@ $("#tecla-multiplicar").click(function() {
 
 $("#tecla-dividir").click(function() {
 	dividir();
-});*/
+});
 
 
 
-//Registro números con teclado
+//Registro números con teclado con disposición ES
 $(document).keydown(function() {
 	const tecla = event.keyCode;
 	const tecla_valor = event.key;
+
+	//Registro números por consola para mapear teclas
 	//console.log("tecla " + tecla + " valor " + tecla_valor);
 
-	/* Códigos
+	/* Códigos en numeral y no numeral
 	/	111
-	*	106
-	+	107
-	-	109
-	7	103
-	8	104
-	9	105
-	4	100
-	5	101
-	6	102
-	1	97
-	2	98
-	3	99
-	0	96
-	.	110
+	*	106	
+	+	107	171
+	-	109	173
+	7	103	55
+	8	104	56
+	9	105	57
+	4	100	52
+	5	101	53
+	6	102	54
+	1	97	49
+	2	98	50
+	3	99	51
+	0	96	48
+	.	110	190
 	Intro 13*/
+
 	switch(tecla) {
 
 		case 107: //Tecla +
@@ -123,10 +126,23 @@ $(document).keydown(function() {
 			resultado();
 		break;
 
-		case 110: //Tecla .
+
+
+
+		case 190: //Tecla .
 			num_pantalla += ".";
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
+
+		case 54: //Tecla .
+			num_pantalla += tecla_valor;
+
+			//Imprimimos el valor de la secuencia sin el 0 inicial
+			$("#pantalla-resultados").html(num_pantalla.substring(1));
+		break;
+
+
+
 
 		case 96: //Tecla 0
 			num_pantalla += tecla_valor;
@@ -135,57 +151,190 @@ $(document).keydown(function() {
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
 
+		case 48: //Tecla 0
+			num_pantalla += tecla_valor;
+
+			//Imprimimos el valor de la secuencia sin el 0 inicial
+			$("#pantalla-resultados").html(num_pantalla.substring(1));
+		break;
+
+
+
+
 		case 97: //Tecla 1
 			num_pantalla += tecla_valor;
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
+
+		case 49: //Tecla 1
+			num_pantalla += tecla_valor;
+
+			//Imprimimos el valor de la secuencia sin el 0 inicial
+			$("#pantalla-resultados").html(num_pantalla.substring(1));
+		break;
+
+
+
 
 		case 98: //Tecla 2
 			num_pantalla += tecla_valor;
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
 
+		case 50: //Tecla 2
+			num_pantalla += tecla_valor;
+
+			//Imprimimos el valor de la secuencia sin el 0 inicial
+			$("#pantalla-resultados").html(num_pantalla.substring(1));
+		break;
+
+
+
+
 		case 99: //Tecla 3
 			num_pantalla += tecla_valor;
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
+
+
+		case 51: //Tecla 3
+			num_pantalla += tecla_valor;
+
+			//Imprimimos el valor de la secuencia sin el 0 inicial
+			$("#pantalla-resultados").html(num_pantalla.substring(1));
+		break;
+
+
+
 
 		case 100: //Tecla 4
 			num_pantalla += tecla_valor;
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
 
+		case 52: //Tecla 4
+			num_pantalla += tecla_valor;
+
+			//Imprimimos el valor de la secuencia sin el 0 inicial
+			$("#pantalla-resultados").html(num_pantalla.substring(1));
+		break;
+
+
+
+
 		case 101: //Tecla 5
 			num_pantalla += tecla_valor;
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
+
+		case 53: //Tecla 5
+			num_pantalla += tecla_valor;
+
+			//Imprimimos el valor de la secuencia sin el 0 inicial
+			$("#pantalla-resultados").html(num_pantalla.substring(1));
+		break;
+
+
+
 
 		case 102: //Tecla 6
 			num_pantalla += tecla_valor;
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
 
+		case 54: //Tecla 6
+			num_pantalla += tecla_valor;
+
+			//Imprimimos el valor de la secuencia sin el 0 inicial
+			$("#pantalla-resultados").html(num_pantalla.substring(1));
+		break;
+
+
+
+
 		case 103: //Tecla 7
 			num_pantalla += tecla_valor;
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
+
+		case 55: //Tecla 7
+			num_pantalla += tecla_valor;
+
+			//Imprimimos el valor de la secuencia sin el 0 inicial
+			$("#pantalla-resultados").html(num_pantalla.substring(1));
+		break;
+
+
+
 
 		case 104: //Tecla 8
 			num_pantalla += tecla_valor;
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
 
+		case 56: //Tecla 8
+			num_pantalla += tecla_valor;
+
+			//Imprimimos el valor de la secuencia sin el 0 inicial
+			$("#pantalla-resultados").html(num_pantalla.substring(1));
+		break;
+
+
+
+
 		case 105: //Tecla 9
 			num_pantalla += tecla_valor;
 			$("#pantalla-resultados").html(num_pantalla.substring(1));
 		break;
 
+		case 57: //Tecla 9
+			num_pantalla += tecla_valor;
+
+			//Imprimimos el valor de la secuencia sin el 0 inicial
+			$("#pantalla-resultados").html(num_pantalla.substring(1));
+		break;
+
+
+
+
 		case 144: //Tecla bloq
 			reset();
 		break;
 
+		case 67: //Tecla C
+			reset();
+		break;
+
+
+
+
 		default:
-		console.log("Tecla incorrecta");
+		//console.log("Tecla incorrecta");
+	}
+
+
+
+
+	switch(tecla_valor) {
+
+		case "+":
+			sumar();
+		break;
+
+		case "-":
+			restar();
+		break;
+
+		case "*":
+			multiplicar();
+		break;		
+
+		case "/":
+			dividir();
+		break;
+		
+		default:
+		//console.log("Tecla incorrecta");
 	}
 });
 
@@ -339,41 +488,7 @@ function dividir() {
 
 
 function resultado() {
-/*
-	let valor_pantalla = $("#pantalla-resultados").text();
-	let num_previo = parseInt(valor_pantalla);
-	
-	if(fila_1 !== 0) { 
-		
-		fila_2 = num_previo;
-
-		//Para imprimir el resultado, sumamos un 0
-		let resultado = fila_1 + 0;
-
-		$("#pantalla-previa").html(resultado);
-		$("#pantalla-resultados").html(resultado);
-
-		fila_1 = resultado;
-		fila_2 = 0;
-		num_pantalla = 0;
-
-
-	} else {
-
-		fila_1 = num_previo;
-		fila_2 = 0;
-
-		let resultado = fila_1 + fila_2;
-
-		$("#pantalla-previa").html(resultado);
-		$("#pantalla-resultados").html(resultado);
-
-		fila_1 = resultado;
-		fila_2 = 0;
-		num_pantalla = 0;
-
-	}
-	*/
+	console.log("Imprimir resultado");
 }
 
 
